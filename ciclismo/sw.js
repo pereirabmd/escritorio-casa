cd0f87bf79cef404977babbc77aab566d81cb8da
pereirabmd <pereirabmd@gmail.com>
2026-07-28 23:18:39 +0100

    Add files via upload

---
+++ ciclismo/sw.js
@@ -0,0 +1,31 @@
+var CACHE = 'ciclismo-shell-v2';
+var SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(cache){ return cache.addAll(SHELL); }));
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      var network = fetch(e.request).then(function(res){
+        if (res && res.status === 200){
+          var copy = res.clone();
+          caches.open(CACHE).then(function(cache){ cache.put(e.request, copy); });
+        }
+        return res;
+      }).catch(function(){ return cached; });
+      return cached || network;
+    })
+  );
+});
