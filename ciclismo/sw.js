56a33808d9d2399bb22b4cd168c0c75d9846d347
pereirabmd <pereirabmd@gmail.com>
2026-08-23 18:18:34 +0100

    Add files via upload

--- ciclismo/sw.js
+++ ciclismo/sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'ciclismo-shell-v12';
+var CACHE = 'ciclismo-shell-v14';
 var SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-watermark.png'];
 
 self.addEventListener('install', function(e){
